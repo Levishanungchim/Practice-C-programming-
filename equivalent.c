35e2c4990c67fb8350ef5753f7e727a5952cd1a9
Levishanungchim <109846961+Levishanungchim@users.noreply.github.com>
2022-07-23 14:18:49 +0530

    Add files via upload

---
+++ equivalent.c
@@ -0,0 +1,8 @@
+ #include<stdio.h>
+ int main()
+ {int a ;
+ printf("Enter the decimal number\n");
+ scanf("%d",&a);
+ printf("The equivalent octal and hexidecimal value of %d is %0 and %x\n ",a,a,a);
+ return 0;
+ }
