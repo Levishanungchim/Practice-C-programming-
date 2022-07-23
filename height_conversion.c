750d1fd01e694d25003ac7975caaa1f0ec5c71cb
Levishanungchim <109846961+Levishanungchim@users.noreply.github.com>
2022-07-23 14:27:21 +0530

    Add files via upload

---
+++ height_conversion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{int a,feet;
+float rem,inch;
+printf("Enter height in cm\n");
+scanf("%d",&a);
+feet=a/30;
+rem=a%30;
+inch=rem/2.5;
+printf("The value of %d is %d\'%.2f\"\n",a,feet,inch);
+return;
+}
+
