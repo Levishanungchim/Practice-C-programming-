ed21cb79deeb42d0a506a558cfac8e279471a5d7
Levishanungchim <109846961+Levishanungchim@users.noreply.github.com>
2022-07-23 14:10:26 +0530

    Add files via upload

---
+++ Product.c
@@ -0,0 +1,10 @@
+#include<stdio.h.>
+int main()
+{
+	float a,b,product;
+	printf("Enter any two numbers\n");
+	scanf("%f%f",&a,&b);
+	product=a*b;
+	printf("The value of  product of two given number is %f\n ",product);
+	return 0;
+}
